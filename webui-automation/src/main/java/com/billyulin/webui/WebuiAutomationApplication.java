79f0a7f3f10c1398cefcef33a50080d687fa4244
Yummuu <info@liyulinbill.com>
2024-04-04 09:55:11 +0800

    init

---
+++ webui-automation/src/main/java/com/billyulin/webui/WebuiAutomationApplication.java
@@ -0,0 +1,13 @@
+package com.billyulin.webui;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebuiAutomationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebuiAutomationApplication.class, args);
+    }
+
+}
---
+++ webui-automation/src/test/java/com/billyulin/webui/WebuiAutomationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.billyulin.webui;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebuiAutomationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
